f6e2ad9a4537e6c55a175dc1ac3aad2a9a2190e1
David dos Santos Machado <david_machado@ufrrj.br>
2022-01-16 19:10:27 -0300

    Atualizando a URL do git

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,13 +11,13 @@ function Navbar() {
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
           <a
-            href="https://github.com/devsuperior"
+            href="https://github.com/davidOdahcam"
             target="_blank"
             rel="noreferrer"
           >
             <div className="dsmovie-contact-container">
               <GithubIcon />
-              <p className="dsmovie-contact-link">/devsuperior</p>
+              <p className="dsmovie-contact-link">/davidOdahcam</p>
             </div>
           </a>
         </div>
