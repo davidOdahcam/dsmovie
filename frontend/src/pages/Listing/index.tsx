6dfff8e5dae0e350776b1c18771eee18b8dd63b2
David dos Santos Machado <david_machado@ufrrj.br>
2022-01-16 17:31:22 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,12 +1,22 @@
+import { useState, useEffect } from 'react';
 import axios from "axios";
+
+// COMPONENTS
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
-  axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {
-    console.log(response.data);
-  })
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {
+      const data = response.data as MoviePage;
+      console.table(data)
+      setPageNumber(data.number);
+    });
+  }, []);
 
   return (
     <>
